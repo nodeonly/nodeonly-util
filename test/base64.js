01e24c7707ae03eb6d7c833d122b765f6449d574
alfred sang <i5ting@126.com>
2014-10-18 14:02:21 +0800

    refact test code to bdd style

--- test/base64.js
+++ test/base64.js
@@ -4,18 +4,24 @@ require('chai').should();
 
 var Base64 = require('../lib/base64');
 
-suite('Base64', function() {
-     test("base64_encode", function() {
-		 var str = "nodeonly"
-		 var result_str = Base64.base64_encode(str);
 
-		 assert.equal(result_str, 'bm9kZW9ubHk=');
-     })
-	 
-     test("base64_decode", function() {
-		 var base64_encode_str = "bm9kZW9ubHk"
-		 var result_str = Base64.base64_decode(base64_encode_str);
+describe('Base64', function(){
+  describe('#base64_encode()', function(){
+    it('should return bm9kZW9ubHk= when base64_encode nodeonly', function(){
+	 	 	var str = "nodeonly"
+	 	 	var result_str = Base64.base64_encode(str);
 
-		 assert.equal(result_str, 'nodeonly');
-     })
+			assert.equal(result_str, 'bm9kZW9ubHk=');
+    })
+  })
+  
+  describe('#base64_decode()', function(){
+    it('should return nodeonly when base64_decode bm9kZW9ubHk=', function(){
+			var base64_encode_str = "bm9kZW9ubHk"
+			var result_str = Base64.base64_decode(base64_encode_str);
+
+			assert.equal(result_str, 'nodeonly');
+    })
+  })
+  
 })
