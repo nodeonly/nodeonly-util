394de4cc9f4909df3890b623603c169711a1fd4d
alfred sang <i5ting@126.com>
2014-11-01 21:05:16 +0800

    req test

--- test/req.js
+++ test/req.js
@@ -4,14 +4,36 @@ require('chai').should();
 
 var RequestUtils = require('../lib/req');
 
+var request = require('supertest')
+  , express = require('express');
 
-describe('Req', function(){
+
+describe('RequestUtils', function(){
   describe('#get_value_from_body()', function(){
-    it('should return bm9kZW9ubHk= when base64_encode nodeonly', function(){
-			
-	 	 	var result_str = RequestUtils.get_value_from_body(req,'name');
+    it('should return Manny when get_value_from_body with key name', function(done){
 
-			assert.equal(result_str, 'bm9kZW9ubHk=');
+			var app = express();
+			
+			app.post('/user', function(req, res){
+				// mock req.body data
+				req.body = { 'name': 'Manny', 'species': 'cat' };
+				
+				var species = RequestUtils.get_value_from_body(req, 'name');
+				assert.equal(species, 'Manny');
+				
+		 	 	var species = RequestUtils.get_value_from_body(req, 'species');
+				assert.equal(species, 'cat');
+				
+				done();
+			});
+			
+			request(app)
+			  .post('/user')
+				.set('contentType', 'application/x-www-form-urlencoded; charset=utf-8')
+			  .end(function(err, res){
+					if (err) throw err;
+			  });
+
     })
   })
   //
